29dfd8b922bad7f68ac79bdf8e024abb8cd1d9b1
Valentin Cheillan <valentin.cheillan@etu.minesparis.psl.eu>
2025-10-28 16:23:16 +0100

    V7 De plus en plus stylé

--- messenger.py
+++ messenger.py
@@ -1,5 +1,6 @@
 from datetime import datetime
 import random 
+import json
 
 server = {
     'users': [
@@ -19,8 +20,11 @@ server = {
         }
     ]
 }
+#my_server = json.loads(server)
 liste_id = [dico['id'] for dico in server['users']]
 Availables_id = [ i for i in range(1000) if i not in liste_id]
+liste_idg = [dico['id'] for dico in server['channels']]
+Availables_idg = [ i for i in range(1000) if i not in liste_id]
 def menu_principal(choice):
     MP = input('Do you want to go to the main menu ? ')
     if MP == 'Yes':
@@ -28,16 +32,22 @@ def menu_principal(choice):
     
 def choix():
     print('=== Messenger ===')
-    print('x. Leave. u. g')
+    print('x. Leave. u. g. au. ag.')
     choice = input('Select an option: ')
     if choice == 'x':
         print('Bye!')
-        choix()
+        Bol = input('Do you want to continue ? :')
+        if Bol == 'Yes':
+           choix()
+        
         #menu_principal(choice)
     elif choice == 'u':
         for dico in server['users']:
             print(dico)
-        choix()
+        Bol = input('Do you want to continue ? :')
+        if Bol == 'Yes':
+           choix()
+        
         # menu_principal(choice)
             
     elif choice == 'g':
@@ -53,19 +63,32 @@ def choix():
                     break
                 else:
                     print('This group doesnt exist')
-        choix()
-    elif choice == 'a':
+        Bol = input('Do you want to continue ? :')
+        if Bol == 'Yes':
+           choix()
+        
+    elif choice == 'au':
         utilisateur = input( 'Name : ')
         id = random.choice(Availables_id)
         server['users'].append({'id': id, 'name' : utilisateur})
         Availables_id.pop(id)
-        choix()
+        print(server['users'])
+        Bol = input('Do you want to continue ? :')
+        if Bol == 'Yes':
+           choix()
         
-
-
-
-    
-
+    elif choice == 'ag':
+        group_name = input( 'Name : ')
+        id_group = random.choice(Availables_idg)
+        id_utilisateurs_group = input( 'Liste : ')
+        server['channels'].append({'id': id_group, 'name' : group_name, 'members_ids' : id_utilisateurs_group})
+        Availables_idg.pop(id)
+        print(server['channels'])
+        Bol = input('Do you want to continue ? :')
+        if Bol == 'Yes':
+           choix()
+            
     else:
         print('Unknown option:', choice)
 
+choix()
